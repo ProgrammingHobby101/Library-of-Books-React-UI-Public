51c641554dd5478af676f2c0dea7712b5aa02897
Nicholas <nsdonaldwebmail@gmail.com>
2024-09-13 21:14:29 -0400

    added rerouting back to homepage if user uses url bar to navigate to BookView page. Also added other miscellaneous changes.

--- src/MyComponents/BookView.js
+++ src/MyComponents/BookView.js
@@ -1,30 +1,50 @@
 import './BookView.css';
 //import BookThumbnail from '../my_images/default-book-thumbnail-bookstack.jpg';
+import { useNavigate } from 'react-router-dom';
+import {useEffect, useRef} from 'react';
 
 export function BookView(props) {
-    console.log("hello world title:"+props.UserBookItem.title);
-    console.log("hello world:"+JSON.stringify(props.UserBookItem));
+    const navigate = useNavigate();//initilize navigation for homepage if needed.
+    const renderAfterCalled = useRef(false);// this paired with useEffect will prevent useEffect from running twice in Dev mode.
+    useEffect(() => { // with the useEffect empty array at end will Code here will run just like componentDidMount so that fetch only loads once
+          checkIfDataExistFromBookItem();
+          renderAfterCalled.current = true;//need this with useEffect this paired with useEffect will prevent useEffect from running twice in Dev mode.
+    }, []);
+    function checkIfDataExistFromBookItem(){
+        console.log("hello world title:"+props.UserBookItem.title);//test prints
+        console.log("hello world:"+JSON.stringify(props.UserBookItem));//test prints
+        console.log("props.UserBookItem"+props.UserBookItem);//test prints
+        var emptyObj = {};//using var for comparison since adding {} to if condition always returns false.
+        console.log("(JSON.stringify(props.UserBookItem) === JSON.stringify(emptyObj)) is: "+(JSON.stringify(props.UserBookItem) === JSON.stringify(emptyObj)));//test print
+        if (JSON.stringify(props.UserBookItem) === JSON.stringify(emptyObj)) {
+            console.log("rerouting back to homepage because props.UserBookItem was empty object because user went to BookView page without using homepage");
+            navigate("/");//route back to homepage
+          }//else do nothing
+        else {
+            console.log("NOT rerouting back to homepage because props.UserBookItem was defined so this means user went to homepage and use the app to navigate to BookView page");
+        }
+    }
     return (
         // <div>
         //     <p>hello world!</p>
         //     <div>{JSON.stringify(props.UserBookItem)}</div>
         // </div>
         <div className="book-container">
-            <h1>Title</h1>
+            <h1 className='my-H-Tags'>Title</h1>
             {props.UserBookItem.title}
-            <h1>Author</h1>
+            <h1 className='my-H-Tags'>Author</h1>
             {props.UserBookItem.author}
-            <h3>Rating</h3>
+            <h3 className='my-H-Tags'>Rating</h3>
             {(props.UserBookItem.rating>1) ?  props.UserBookItem.rating+" Stars":  props.UserBookItem.rating+" Star"}
-            <h3>Reviewer</h3>
+            <h3 className='my-H-Tags'>Reviewer</h3>
             {props.UserBookItem.reviewer}
-            <h3>Created Date</h3>
+            <h4 className='my-H-Tags'>Created Date</h4>
             {props.UserBookItem.createdDate}
-            <h3>Modified Date</h3>
+            <h4 className='my-H-Tags'>Modified Date</h4>
             {(props.UserBookItem.modifiedDate === null) ?  "N/A": props.UserBookItem.modifiedDate}
-            <h1>Summary</h1>
+            <h3 className='my-H-Tags'>Summary</h3>
             {props.UserBookItem.summary}
-            <h1>ID</h1>
+            <h4 className='my-H-Tags'>ID</h4>
             {props.UserBookItem.id}
         </div>
     );
