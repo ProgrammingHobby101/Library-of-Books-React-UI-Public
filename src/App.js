52ff49c0ad4f50a7d8ea1563066bc5e423fc6302
Nicholas <nsdonaldwebmail@gmail.com>
2024-10-23 10:35:58 -0400

    added github pages home route to react root route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
+        <Route path="/Library-of-Books-React-UI-Public"> {/*root path*/}
           <Route index element={<div className="App">
               <header>
                 <div>
