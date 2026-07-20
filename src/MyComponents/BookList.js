b34345d3675afe52d2041ae4f1c7b4678fe097ec
Nicholas <nsdonaldwebmail@gmail.com>
2026-07-20 16:46:35 -0400

    I moved the question/help mark to right of search box on BookList page. need to fix the edit page so that the selected stars is saved on screen even after the edit has been saved. Also I need to Need to work on UI responsiveness for BookList items because it covers up Edit button when small screens. Also need to limit each Booklist's item's maximum title and author capacity.

--- src/MyComponents/BookList.js
+++ src/MyComponents/BookList.js
@@ -221,8 +221,7 @@ export function BookList (props) {
           // zIndex: 2147483647, /* WORKS! always shows search bar, Ensure it's above other content*/
           }}>
             <div  style={{height:"100%", display: "flex", alignItems: "center"}}>
-              <BsQuestionCircle style={{fontSize:"5rem", height:"100%", display: "flex", alignItems: "center"}} onClick={()=>{alert("Hit the \"Create A Review\" button to starting making book reviews")}}/>
-                  
+              
               <div style={{height:"100%", width: "100%", borderRadius: "1rem"}}>
                   
                 <form className="my-form" onSubmit={SearchBoxSubmit} style={{height:" 100%", width: "100%" }}>
@@ -235,6 +234,7 @@ export function BookList (props) {
                     </center>
                 </form>
               </div>
+              <BsQuestionCircle style={{fontSize:"5rem", height:"100%", display: "flex", alignItems: "center"}} onClick={()=>{alert("Hit the \"Create A Review\" button to starting making book reviews")}}/>
 
               </div>
             </header>
